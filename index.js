935fc1f0588a2e57c0299cb9ddf2149e9d549467
Yael Marmon <ymarmon@gmail.com>
2017-11-06 21:07:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = { foo: 'bar' };
+
+function updateObjectWithKeyAndValue(object, key, value){
+ var newObject = Object.assign({}, object, { [key]: value })
+ return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign(object,{[key]:value});
+}
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+ delete object.key
+ return object
+}
